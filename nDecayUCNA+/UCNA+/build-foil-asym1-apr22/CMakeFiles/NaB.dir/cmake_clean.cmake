83033bcc8c5126661b3ef1dcfa4ca8a8aa6e302b
Rashika Gupta <rgu245@mcc-login002.localdomain>
2024-04-24 23:01:15 -0400

    execs for diff As foil2 new pos

---
+++ nDecayUCNA+/UCNA+/build-foil-asym1-apr22/CMakeFiles/NaB.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NaB.dir/newNab.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBAnalysisManager.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBDetectorConstruction.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBDetectorMessenger.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBEventAction.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBField.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBPhysicsList.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBRunAction.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBStackingAction.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBSteppingAction.cc.o"
+  "CMakeFiles/NaB.dir/src/UCNBSteppingVerbose.cc.o"
+  "NaB.pdb"
+  "NaB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NaB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
